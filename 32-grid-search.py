e0a1fe550e0dcb7208ed026b433ea138d90222f0
oguzhan ilikcay <oguzhanilikcay@outlook.com>
2024-01-12 22:41:26 +0300

    .

--- 32-grid-search.py
+++ 32-grid-search.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import mglearn
+import seaborn
 
-
-# a simple grid search with for loop
+# a simple grid search with for loop:
 from sklearn.datasets import load_iris
 iris = load_iris()
 
@@ -43,12 +44,12 @@ plt.show()
 
 from sklearn.svm import SVC
 
-# split data into train+validation set and test set
+# split <data> into train+validation set and test set
 X_trainval, X_test, y_trainval, y_test = train_test_split(
     iris.data, iris.target, random_state=0
 )
 
-# split train+validation set into training and validation sets
+# split <train+validation> set into <training and validation> sets
 X_train, X_valid, y_train, y_valid = train_test_split(
     X_trainval, y_trainval, random_state=1
 )
@@ -127,9 +128,139 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 gs.fit(X_train, y_train)
-print("test set score: {:.2f}".format(gs.score(X_test, y_test)))
-print("best parameters: {}".format(gs.best_params_))
-print("best cross-validation score: {:.2f}".format(gs.best_score_))
 
-print("best estimator:\n{}".format(gs.best_estimator_))
-print("\n\ngrid search results:\n{}".format(gs.cv_results_))
+print("> test set score: {:.2f}".format(gs.score(X_test, y_test)))
+print("> best parameters: {}".format(gs.best_params_))
+print("> best cross-validation score: {:.2f}".format(gs.best_score_))
+print("> combination of parameters: {}".format(gs.param_grid))
+
+print("> best estimator: {}".format(gs.best_estimator_))
+
+# convert cv_results to DataFrame
+results = pd.DataFrame(gs.cv_results_)
+print(results.tail())
+print("column names: {}".format([i for i in results.columns]))
+
+scores = np.array(results.mean_test_score).reshape(6, 6)
+
+
+seaborn.heatmap(
+    scores,
+    xticklabels=param_grid['gamma'],
+    yticklabels=param_grid['C'],
+    cmap="viridis",
+    annot=True)
+
+plt.xlabel('Gamma')
+plt.ylabel('C')
+plt.title('heat map of mean cross-validation score as function of C and gamma')
+
+plt.show()
+
+
+fig, axes = plt.subplots(1, 3, figsize=(13, 5))
+
+param_grid_linear = {'C': np.linspace(1, 2, 6),
+                     'gamma': np.linspace(1, 2, 6)}
+param_grid_one_log = {'C': np.linspace(1, 2, 6),
+                      'gamma': np.logspace(-3, 2, 6)}
+param_grid_range = {'C': np.logspace(-3, 2, 6),
+                    'gamma': np.logspace(-7, -2, 6)}
+
+for param_grid, ax in zip(
+        [param_grid_linear, param_grid_one_log, param_grid_range], axes):
+    grid_search = GridSearchCV(SVC(), param_grid, cv=5)
+    grid_search.fit(X_train, y_train)
+    scores = grid_search.cv_results_['mean_test_score'].reshape(6, 6)
+
+    seaborn.heatmap(
+        scores,
+        xticklabels=param_grid['gamma'],
+        yticklabels=param_grid['C'],
+        cmap='viridis',
+        annot=True,
+        ax=ax
+    )
+
+    plt.xlabel('gamma')
+    plt.ylabel('C')
+
+plt.show()
+
+
+#
+param_grid = [
+    {'kernel': ['rbf'],
+     'C': [0.001, 0.01, 0.1, 1, 10, 100],
+     'gamma': [0.001, 0.01, 0.1, 1, 10, 100]},
+    {'kernel': ['linear'],
+     'C': [0.001, 0.01, 0.1, 1, 10, 100]}
+]
+print("> list of grids:\n{}".format(param_grid))
+
+grid_search = GridSearchCV(SVC(), param_grid, cv=5)
+grid_search.fit(X_train, y_train)
+print("> best parameters: {}".format(grid_search.best_params_))
+print("> best cross-validation score: {:.2}".format(grid_search.best_score_))
+
+results = pd.DataFrame(grid_search.cv_results_)
+print("> cv_results_:\n{}".format(results.T))
+
+#
+# nested cross-validation
+#
+
+scores = cross_val_score(
+    GridSearchCV(SVC(), param_grid, cv=5),
+    iris.data, iris.target, cv=5
+)
+
+print("cross-validation score: ", scores)
+print("mean cross-validation score: ", scores.mean())
+
+print("---"*30)
+
+def nested_cv(X, y, inner_cv, outer_cv, Classifier, parameter_grid):
+    outer_scores = []
+
+    for training_samples, test_samples in outer_cv.split(X, y):
+        best_params = {}
+        best_score = -np.inf
+
+
+        for parameters in parameter_grid:
+            cv_scores = []
+
+            for inner_train, inner_test in inner_cv.split(X[training_samples], y[training_samples]):
+
+                clf = Classifier(**parameters)
+                clf.fit(X[inner_train], y[inner_train])
+
+                score = clf.score(X[inner_test], y[inner_test])
+                cv_scores.append(score)
+
+            mean_score = np.mean(cv_scores)
+
+            if mean_score > best_score:
+                best_score = mean_score
+                best_params = parameters
+
+        clf = Classifier(**best_params)
+        clf.fit(X[training_samples], y[training_samples])
+
+        outer_scores.append(clf.score(X[test_samples], y[test_samples]))
+
+    return np.array(outer_scores)
+
+
+from sklearn.model_selection import ParameterGrid, StratifiedKFold
+
+scores = nested_cv(
+    X=iris.data, y=iris.target,
+    inner_cv=StratifiedKFold(5),
+    outer_cv=StratifiedKFold(5),
+    Classifier=SVC,
+    parameter_grid=ParameterGrid(param_grid)
+)
+
+print("cross-validation scores: {}".format(scores))
---
+++ 34-precision-recall-curves.py
@@ -0,0 +1,147 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import mglearn
+
+from mglearn.datasets import make_blobs
+X, y = make_blobs(
+    n_samples=(400, 50), cluster_std=[7.0, 2], random_state=22
+)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, random_state=0
+)
+
+from sklearn.svm import SVC
+svm = SVC(gamma=0.05)
+svm.fit(X_train, y_train)
+y_pred = svm.predict(X_test)
+
+mglearn.plots.plot_decision_threshold()
+plt.show()
+
+from sklearn.metrics import classification_report
+c_report = classification_report(y_test, y_pred)
+print("classification_report:\n{}".format(c_report))
+
+y_pred_lower_threshold = svm.decision_function(X_test) > -0.8
+print("classification_report:\n{}".format(classification_report(y_test, y_pred_lower_threshold)))
+
+'''
+let's assume in our application it is more important to have a high recall for class 1.
+this means we are willing to risk more false positives (false class 1) in exchance for more
+true positives (which will increase the recall).
+'''
+print("---"*24)
+
+#
+# precision, recall, threshold | ROC - AUC
+#
+
+from sklearn.metrics import precision_recall_curve
+precision0, recall0, thresholds0 = precision_recall_curve(
+    y_test, svm.decision_function(X_test)
+)
+
+
+#
+X, y = make_blobs(
+    n_samples=(4000, 500), cluster_std=[7.0, 2], random_state=22
+)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, random_state=0
+)
+
+svc = SVC(gamma=0.05)
+svc.fit(X_train, y_train)
+
+precision, recall, thresholds = precision_recall_curve(
+    y_test, svc.decision_function(X_test)
+)
+
+close_zero = np.argmin(np.abs(thresholds))
+plt.plot(precision[close_zero], recall[close_zero],
+         'o', markersize=10, label='threshold zero', fillstyle='none', c='k', mew=2)
+
+plt.plot(precision, recall, label='precision recall curve')
+plt.xlabel('precision')
+plt.ylabel('recall')
+plt.title('precison recall curve for SVC(gamma=0.05)')
+
+plt.show()
+
+
+from sklearn.ensemble import RandomForestClassifier
+rfc = RandomForestClassifier(n_estimators=100, random_state=0, max_features=2)
+rfc.fit(X_train, y_train)
+
+precision_rf, recall_rf, thresholds_rf = precision_recall_curve(
+    y_test, rfc.predict_proba(X_test)[:, 1]
+)
+
+plt.plot(precision, recall, label='svc')
+plt.plot(precision[close_zero], recall[close_zero],
+         'o', markersize=10, label='threshold zero svc', fillstyle='none', c='k', mew=2)
+plt.plot(precision_rf, recall_rf, label='random forest')
+
+close_default_rf = np.argmin(np.abs(thresholds_rf - 0.5))
+
+plt.plot(precision_rf[close_default_rf], recall_rf[close_default_rf],
+         '^', markersize=10, label='threshold 0.5 rfc', fillstyle='none', c='k', mew=2)
+
+plt.xlabel('precision')
+plt.ylabel('recall')
+plt.legend(loc='best')
+plt.title('comparing precision recall curves of svm and random forest')
+
+plt.show()
+
+
+from sklearn.metrics import f1_score
+print("f1 - svc: {:.3f}".format(f1_score(y_test, svc.predict(X_test))))
+print("f1 - rfc: {:.3f}".format(f1_score(y_test, rfc.predict(X_test))))
+
+
+from sklearn.metrics import roc_curve
+fpr, tpr, thresholds = roc_curve(y_test, svc.decision_function(X_test))
+
+plt.plot(fpr, tpr, label='ROC curve')
+plt.xlabel('FPR')
+plt.ylabel('TPR (recall)')
+
+close_zero = np.argmin(np.abs(thresholds))
+plt.plot(fpr[close_zero], tpr[close_zero],
+         'o', markersize=10, label='threshold zero', fillstyle='none', c='k', mew=2)
+plt.legend(loc=4)
+plt.title('ROC curve for svm')
+
+plt.show()
+
+
+fpr_rf, tpr_rf, thold_rf = roc_curve(y_test, rfc.predict_proba(X_test)[:, 1])
+
+plt.plot(fpr, tpr, label='ROC curve svc')
+plt.plot(fpr_rf, tpr_rf, label='ROC curve rf')
+
+plt.plot(fpr[close_zero], tpr[close_zero],
+         'o', markersize=10, label='threshold close zero', fillstyle='none', c='k', mew=2)
+
+close_default_rf = np.argmin(np.abs(thresholds_rf - 0.5))
+plt.plot(fpr_rf[close_default_rf], tpr_rf[close_default_rf],
+         '^', markersize=10, label='threshold 0.5 rf', fillstyle='none', c='k', mew=2)
+
+plt.legend(loc=4)
+plt.xlabel('FPR')
+plt.ylabel('TPR (recall)')
+plt.title('comparing ROC curves for svm and random forest')
+
+plt.show()
+
+
+from sklearn.metrics import roc_auc_score
+rf_auc = roc_auc_score(y_test, rfc.predict_proba(X_test)[:, 1])
+svc_auc = roc_auc_score(y_test, svc.decision_function(X_test))
+
+print("AUC for rfc: {:.2f}".format(rf_auc))
+print("AUC for svc: {:.2f}".format(svc_auc))
